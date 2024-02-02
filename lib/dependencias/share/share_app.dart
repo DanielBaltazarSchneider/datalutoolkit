118dc3e6c962e54b07cbef65033cbaabed1543fc
Daniel Schneider <daniel.schneider@agrotis.com>
2024-02-02 08:02:46 -0300

    feat: altera metodo share

--- lib/dependencias/share/share_app.dart
+++ lib/dependencias/share/share_app.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 import 'package:share_plus/share_plus.dart';
 
 class ShareApp {
-  Future<void> shareFile(File file) async {
-    final result = await Share.shareXFiles([XFile(file.path)], text: 'Recomendação de calagem');
+  Future<void> shareFile(File file, {String? filename}) async {
+    final result = await Share.shareXFiles([XFile(file.path)], text: filename ?? 'Recomendação de calagem');
 
     if (result.status == ShareResultStatus.dismissed) {
       print('Did you not like the pictures?');
